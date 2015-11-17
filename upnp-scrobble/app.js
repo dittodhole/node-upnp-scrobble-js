af333712169208f7dda43c01688eb6879056aaee
Andreas Niedermair <andreas@niedermair.name>
2015-11-18 00:13:25 +0100

    demo init

---
+++ upnp-scrobble/app.js
@@ -0,0 +1,17 @@
+﻿var config = require("./config.json");
+var upnpClient = require("node-upnp-client");
+var cli = new upnpClient();
+cli.searchDevices();
+cli.on("searchDevicesEnd", function () {
+    console.log("Servers" + JSON.stringify(cli._renderers));
+});
+//var Scribble = require("scribble");
+//var MediaRendererClient = require('upnp-mediarenderer-client');
+//urn:schemas-upnp-org:device:MediaRenderer:1
+
+//var scribble = new Scribble(config.lastfm.key,
+//                            config.lastfm.secret,
+//                            config.lastfm.username,
+//                            config.lastfm.password);
+
+console.log("Hello world");
