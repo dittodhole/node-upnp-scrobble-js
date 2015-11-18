86629c7b1a0e721f3e1710a1028c2890cbb9a6fb
Andreas Niedermair <andreas@niedermair.name>
2015-11-18 01:09:07 +0100

    finished basic implementation, quite unstable

--- upnp-scrobble/app.js
+++ upnp-scrobble/app.js
@@ -1,17 +1,38 @@
 ﻿var config = require("./config.json");
-var upnpClient = require("node-upnp-client");
-var cli = new upnpClient();
-cli.searchDevices();
-cli.on("searchDevicesEnd", function () {
-    console.log("Servers" + JSON.stringify(cli._renderers));
+var MediaRendererClient = require("upnp-mediarenderer-client");
+var Scribble = require("scribble");
+var parseString = require("xml2js").parseString;
+var Client = require("node-ssdp").Client;
+
+var scribble = new Scribble(config.lastfm.key,
+    config.lastfm.secret,
+    config.lastfm.username,
+    config.lastfm.password);
+
+var client = new Client();
+client.on("response", function (headers, statusCode, rinfo) {
+    var st = headers.ST;
+    if (st != "urn:schemas-upnp-org:device:MediaRenderer:1") {
+        return;
+    }
+
+    var location = headers.LOCATION;
+    console.log("found: " + location);
+
+    var mediaRendererClient = new MediaRendererClient(location);
+    mediaRendererClient.on("status", function (status) {
+        parseString(status.AVTransportURIMetaData, function (error, result) {
+            console.log(result);
+
+            var play = {
+                "artist": result["DIDL-Lite"].item[0]["upnp:artist"][0],
+                "track": result["DIDL-Lite"].item[0]["dc:title"][0],
+                "album": result["DIDL-Lite"].item[0]["upnp:album"][0]
+            };
+            scribble.Scrobble(play, function (response) {
+                console.log(response);
+            });
+        });
+    });
 });
-//var Scribble = require("scribble");
-//var MediaRendererClient = require('upnp-mediarenderer-client');
-//urn:schemas-upnp-org:device:MediaRenderer:1
-
-//var scribble = new Scribble(config.lastfm.key,
-//                            config.lastfm.secret,
-//                            config.lastfm.username,
-//                            config.lastfm.password);
-
-console.log("Hello world");
+client.search("urn:schemas-upnp-org:device:MediaRenderer:1");
