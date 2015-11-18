a4810dec52f8eaa39211367cccf352dd62013633
Andreas Niedermair <andreas@niedermair.name>
2015-11-18 20:16:30 +0100

    restructured project
    split giant method into more fine grained scenario
    added interval for scanning network

--- upnp-scrobble/app.js
+++ upnp-scrobble/app.js
@@ -1,40 +1,59 @@
-﻿var config = require("./config.json");
+﻿var Scribble = require("scribble");
+var Parser = require("xml2js");
+var SSDP = require("node-ssdp");
 var MediaRendererClient = require("upnp-mediarenderer-client");
-var Scribble = require("scribble");
-var parseString = require("xml2js").parseString;
-var Client = require("node-ssdp").Client;
 
+var config = require("./config.json");
 var scribble = new Scribble(config.lastfm.key,
-                            config.lastfm.secret,
-                            config.lastfm.username,
-                            config.lastfm.password);
+    config.lastfm.secret,
+    config.lastfm.username,
+    config.lastfm.password);
 
-var client = new Client();
-client.on("response", function (headers, statusCode, rinfo) {
-    // TODO if the search is stable, no additional check against ST is needed anymore
-    var st = headers.ST;
-    if (st != "urn:schemas-upnp-org:device:MediaRenderer:1") {
-        return;
-    }
+function prettyJson(obj) {
+    return JSON.stringify(obj, null, 2);
+};
 
-    var location = headers.LOCATION;
-    console.log("found: " + location);
+var devices = {};
 
-    var mediaRendererClient = new MediaRendererClient(location);
+function scanNetwork() {
+    var client = new SSDP.Client();
+    client.on("response", function (device) {
+        // TODO if the search is stable, no additional check against ST is needed anymore
+        var st = device.ST;
+        if (st != "urn:schemas-upnp-org:device:MediaRenderer:1") {
+            return;
+        }
+
+        if (devices[device.LOCATION]) {
+            return;
+        }
+
+        console.log("found a device: " + prettyJson(device));
+
+        devices[device.LOCATION] = initializeDevice(device);
+    });
+
+    // TODO make the search stable
+    client.search("urn:schemas-upnp-org:device:MediaRenderer:1");
+};
+
+function initializeDevice(device) {
+    var mediaRendererClient = new MediaRendererClient(device.LOCATION);
     mediaRendererClient.on("status", function (status) {
-        parseString(status.AVTransportURIMetaData, function (error, result) {
-            console.log(result);
+        if (status.TransportState !== "PLAYING") {
+            return;
+        }
 
-            if (!result) {
+        Parser.parseString(status.AVTransportURIMetaData, function (error, result) {
+            if (error) {
+                console.log("error occured during parsing: " + prettyJson(error));
                 return;
             }
 
-            // TODO remove namespaces in xml
-            var play = {
-                "artist": result["DIDL-Lite"].item[0]["upnp:artist"][0],
-                "track": result["DIDL-Lite"].item[0]["dc:title"][0],
-                "album": result["DIDL-Lite"].item[0]["upnp:album"][0]
-            };
+            var play = parseResult(result);
+            if (!play) {
+                return;
+            }
 
             scribble.Scrobble(play, function (response) {
                 console.log(response);
@@ -42,7 +61,46 @@ client.on("response", function (headers, statusCode, rinfo) {
             // TODO scrobble occurs immediately - usual experience: scrobble is triggered at around 80% of the position.
         });
     });
-});
 
-// TODO make the search stable
-client.search("urn:schemas-upnp-org:device:MediaRenderer:1");
+    return device;
+};
+
+const resultParsers = {
+    "DIDL-Lite": function (result) {
+        var play = {
+            "artist": result.item[0]["upnp:artist"][0],
+            "track": result.item[0]["dc:title"][0],
+            "album": result.item[0]["upnp:album"][0]
+        };
+        return play;
+    }
+};
+
+function parseResult(result) {
+    console.log("trying to find a parser for: " + prettyJson(result));
+
+    for (var key in result) {
+        if (!result.hasOwnProperty(key)) {
+            continue;
+        }
+
+        if (resultParsers.hasOwnProperty(key)) {
+            var resultParser = resultParsers[key];
+            // TODO clarify if we need the inner result for other scenarios, or not ... ? :bomb:
+            var innerResult = result[key];
+            var play = resultParser(innerResult);
+
+            console.log("parsed to following object: " + prettyJson(play));
+
+            return play;
+        }
+    }
+
+    return null;
+};
+
+console.log("Hi");
+
+const intervalTimeout = 30 * 1000;
+setInterval(scanNetwork, intervalTimeout);
+scanNetwork();
