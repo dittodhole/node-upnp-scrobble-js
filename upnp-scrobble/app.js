6ec493646496ca576cc7a0b0df99388d322dbb48
Andreas Niedermair <andreas@niedermair.name>
2015-11-27 20:45:43 +0100

    major overhaul, now using peer-upnp

--- upnp-scrobble/app.js
+++ upnp-scrobble/app.js
@@ -1,102 +1,107 @@
 ﻿var Bunyan = require('bunyan');
-var SSDP = require('node-ssdp');
+var http = require('http');
 var _ = require('underscore');
+var xml2js = require('xml2js');
+var xmlParser = new xml2js.Parser({
+  'mergeAttrs': true,
+  'explicitArray': false,
+  'ignoreXmlns': true
+});
 var objectPath = require('object-path');
 var Scribble = require('scribble');
-var upnpDevice = require('node-upnp-device-js');
-
-var upnpMessage = require('./lib/upnp-message.js');
 
 var config = require('./config.json');
 
 'use strict';
 
-var log = Bunyan.createLogger({
-  "name": 'upnp-scrobble',
-  "level": config.logLevel
-});
-
 function prettyJson(obj) {
   return JSON.stringify(obj, null, 2);
 };
 
-function scanNetwork() {
-  // TODO make the search stable
-  const client = new SSDP.Client({
-    "logLevel": config.logLevel
-  });
-  client.on('response', handleDevice);
-  client.search(config.deviceType);
-};
-
-var devices = {};
-
-function handleDevice(device) {
-  if (device.ST !== config.deviceType) {
-    return;
-  }
-
-  const storedDevice = devices[device.USN];
-  if (!storedDevice || storedDevice.LOCATION !== device.LOCATION) {
-    log.info('Adding device',
-      device);
-    devices[device.USN] = device;
-    initializeDevice(device);
-  }
-};
-
-function initializeDevice(device) {
-  upnpDevice.subscribe(device.LOCATION, config.serviceType, (error, message) => {
-    if (error) {
-      log.error(error,
-        'Received an error on subscription of service % at %',
-        config.serviceType,
-        device.LOCATION);
-      delete device[device.USN];
-    } else if (message) {
-      upnpMessage.getEventMetadata(message, (error, metadata) => {
-        if (error) {
-          log.warn(error,
-            'Parameter "message" has no child-element "Metadata"',
-            prettyJson(message));
-        } else if (metadata) {
-          const play = {
-            'artist': objectPath.get(metadata, 'DIDL-Lite.item.upnp:artist'),
-            'track': objectPath.get(metadata, 'DIDL-Lite.item.dc:title'),
-            'album': objectPath.get(metadata, 'DIDL-Lite.item.upnp:album')
-          };
-          if (!_.isEqual(device.lastPlay, play)) {
-            device.lastPlay = play;
-
-            // TODO scrobble occurs immediately - usual experience: scrobble is triggered at around 80% of the position.
-            const scribble = new Scribble(config.lastfm.key,
-              config.lastfm.secret,
-              config.lastfm.username,
-              config.lastfm.password);
-            scribble.Scrobble(device.lastPlay, (response) => log.info('scrobbled a play', response));
-          }
-        } else {
-          log.warn('Parameter "message" has no child-element "Metadata"',
-            prettyJson(message));
-        }
-      });
-    } else {
-      log.warn('Received empty metadata',
-        prettyJson(message));
-    }
-  }, config.subscriptionTimeoutInSeconds);
-};
+var log = Bunyan.createLogger({
+  'name': 'upnp-scrobble',
+  'level': config.logLevel
+});
 
 log.info('Hi');
 
-// unhandled exceptions should be handled by FOREVER, restarting the app
-//process.on('uncaughtException', (error) => log.error(error));
+var devices = {};
 
-if (config.devices) {
-  config.devices.forEach((device) => handleDevice(device));
-} else {
-  setInterval(scanNetwork,
-    config.scanInterval);
+var server = http.createServer();
+server.listen(config.serverPort);
 
-  scanNetwork();
-}
+var upnp = require('peer-upnp');
+upnp.createPeer({
+  'server': server
+}).on('ready', function (peer) {
+  peer.on(config.serviceType, function (service) {
+    log.info('Found a service',
+      service.USN);
+
+    if (devices[service.USN]) {
+      return;
+    }
+
+    var lastPlay = {};
+    service/*.bind(function (service) {
+      const b = 1;
+    })*/.on('event', function (data) {
+      log.info('Received an event from service',
+        service.USN,
+        prettyJson(data));
+
+      var lastChange = data.LastChange;
+      if (!_.isString(lastChange)) {
+        return;
+      }
+
+      xmlParser.parseString(lastChange, function (error, data) {
+        if (error) {
+          log.warn(error,
+            'Could not parse lastChange',
+            lastChange);
+          return;
+        }
+
+        var metadata = objectPath.get(data, 'Event.InstanceID.AVTransportURIMetaData.val');
+        if (!metadata) {
+          log.warn('No metadata inside',
+            prettyJson(data));
+          return;
+        }
+
+        xmlParser.parseString(metadata, function (error, data) {
+          if (error) {
+            log.error(error,
+              'Could not parse metadata',
+              metadata);
+            return;
+          }
+
+          const play = {
+            'artist': objectPath.get(data, 'DIDL-Lite.item.upnp:artist'),
+            'track': objectPath.get(data, 'DIDL-Lite.item.dc:title'),
+            'album': objectPath.get(data, 'DIDL-Lite.item.upnp:album')
+          };
+
+          if (_.isEqual(lastPlay, play)) {
+            log.warn('Already scrobbled play',
+              prettyJson(play));
+            return;
+          }
+
+          lastPlay = play;
+
+          // TODO scrobble occurs immediately - usual experience: scrobble is triggered at around 80% of the position.
+          const scribble = new Scribble(config.lastfm.key,
+            config.lastfm.secret,
+            config.lastfm.username,
+            config.lastfm.password);
+          scribble.Scrobble(lastPlay, (response) => log.info('scrobbled a play', response));
+        });
+      });
+    }).on('disappear', function (service) {
+      delete devices[service.USN];
+    });
+  });
+}).start();
