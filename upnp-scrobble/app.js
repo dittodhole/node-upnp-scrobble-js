5d6d9a9b3094c86e5d93ffe509272c5b2607a46f
Andreas Niedermair <andreas@niedermair.name>
2016-01-16 22:22:04 +0100

    adapted path for albumArtURI

--- upnp-scrobble/app.js
+++ upnp-scrobble/app.js
@@ -153,7 +153,7 @@ function handleEvent(data, service) {
           "track": objectPath.get(data, 'DIDL-Lite.item.dc:title'),
           "album": objectPath.get(data, 'DIDL-Lite.item.upnp:album'),
           "duration": objectPath.get(data, 'DIDL-Lite.item.res.duration'),
-          "albumArtURI": objectPath.get(data, 'DIDL-Lite.item.upnp:albumArtURI')
+          "albumArtURI": objectPath.get(data, 'DIDL-Lite.item.upnp:albumArtURI._')
         };
 
         service.cancelScrobbling();
