98c2bd91b3a736be902cba6be203415fd1d6f183
Andreas Niedermair <andreas@niedermair.name>
2016-01-19 09:38:28 +0100

    fixes #24

--- upnp-scrobble/app.js
+++ upnp-scrobble/app.js
@@ -31,6 +31,7 @@ handlebars.registerHelper('formatTime', function (time) {
 
 var container = {
   "statusPageMeterUpdateIntervalInSeconds": config.statusPageMeterUpdateIntervalInSeconds || 1,
+  "statusPageRefreshAfterPlayTimeoutInSeconds": config.statusPageRefreshAfterPlayTimeoutInSeconds || 2,
   "scribble": new Scribble(config.lastfm.key, config.lastfm.secret, config.lastfm.username, config.lastfm.password),
   "server": http.createServer()
     .on('request', function (req, res) {
