cd8c0c779e69b3c8ad83d3db43f144e4586defde
Andreas Niedermair <andreas@niedermair.name>
2016-01-20 18:49:20 +0100

    refs #37

---
+++ upnp-scrobble/lib/songStorage.js
@@ -0,0 +1,22 @@
+﻿var events = require('events');
+var utils = require('utils');
+
+function songStorage() {
+  events.EventEmitter.call(this);
+
+  var storage = {};
+  this.clearSong = function (deviceKey) {
+    storage.delete(deviceKey);
+    this.emit('cleared', deviceKey);
+  };
+  this.setSong = function (deviceKey, song) {
+    storage[deviceKey] = song;
+    this.emit('set', deviceKey, song);
+  };
+
+  return this;
+};
+
+utils.inherit(songStorage, events);
+
+module.exports = songStorage;
---
+++ upnp-scrobble/lib/webServer.js
@@ -0,0 +1,37 @@
+﻿var http = require('http');
+var _ = require('underscore');
+var pd = require('pretty-data2').pd;
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+function webServer(port, dataMap) {
+  var hbsConfig = {
+    helpers: {
+      formatXML: function (data) {
+        return pd.xml(data);
+      },
+      formatTime: function (time) {
+        if (time) {
+          return new Date(time).toISOString();
+        }
+        return null;
+      }
+    }
+  };
+  _.extend(hbsConfig.helpers, require('diy-handlebars-helpers'));
+
+  var app = express();
+  app.set('view engine', 'hbs');
+  app.engine('hbs', exphbs(hbsConfig));
+  app.get('/', function (request, response) {
+    var viewName = 'index';
+    var data = dataMap[viewName];
+    response.render('index', data);
+  });
+
+  http.createServer(app).listen(port || 8080);
+
+  return this;
+};
+
+module.exports = webServer;
