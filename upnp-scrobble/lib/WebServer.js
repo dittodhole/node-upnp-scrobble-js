522f0ae7731c402006e04194c9ca6c8c415287e6
Andreas Niedermair <andreas@niedermair.name>
2016-01-25 21:58:56 +0100

    removed unneeded handlebars helpers

--- upnp-scrobble/lib/WebServer.js
+++ upnp-scrobble/lib/WebServer.js
@@ -19,24 +19,9 @@ class WebServer extends EventEmitter {
     this._initialize();
   }
   _initialize() {
-    let hbsConfig = {
-      "helpers": {
-        "formatXML": (data) => {
-          return pd.xml(data);
-        },
-        "formatTime": (time) => {
-          if (time) {
-            return new Date(time).toISOString();
-          }
-          return null;
-        }
-      }
-    };
-    _.extend(hbsConfig.helpers, require('diy-handlebars-helpers'));
-
     this._app = express();
     this._app.set('view engine', 'hbs');
-    this._app.engine('hbs', exphbs(hbsConfig));
+    this._app.engine('hbs', exphbs());
     this._app.get('/', (request, response) => this._renderView('index', request, response));
     this._app.use(express.static('bower_components'));
     this._server = http.createServer(this._app).listen(this._port);
